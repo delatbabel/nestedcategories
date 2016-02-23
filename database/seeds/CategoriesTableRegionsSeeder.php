990ee9a47eef59211f59f21388544defe8482aea
Del <del@babel.com.au>
2016-02-23 15:31:54 +0800

    Add partial list of all regions

---
+++ database/seeds/CategoriesTableRegionsSeeder.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Class CategoriesTableAustraliaRegionsArraySeeder
+ */
+
+use Delatbabel\NestedCategories\Models\Category;
+
+/**
+ * Class CategoriesTableRegionsArraySeeder
+ *
+ * This is an example of a category seeder that is based on the included
+ * CategoriesTableBaseArraySeeder class.  It seeds the database with a heirarchical
+ * list of regions.
+ *
+ * @link https://en.wikipedia.org/wiki/List_of_regions_of_Australia
+ */
+class CategoriesTableRegionsSeeder extends CategoriesTableBaseListSeeder
+{
+    /**
+     * Return a list.
+     *
+     * Over-ride this function in your real seeder class.
+     *
+     * @return array
+     */
+    protected function getNodes()
+    {
+        /**
+         * Get the file contents.
+         *
+         * Read a text file in markdown nested list syntax and return an array
+         * containing the file contents.
+         */
+        $load_file = __DIR__ . '/examples/all-regions.txt';
+        $load_data = file($load_file);
+
+        return $load_data;
+    }
+}
